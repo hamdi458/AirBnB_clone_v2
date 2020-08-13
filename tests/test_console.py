575945fcbfef5ce879661930e7260824f556cc2c
hamdi458 <hamdi.645@gmail.com>
2020-08-14 00:01:29 +0100

    test

--- tests/test_console.py
+++ tests/test_console.py
@@ -14,7 +14,7 @@ class TestConsole(TestCase):
         """unitest"""
         pass
 
-    def test_rem(self):
+    def tst1(self):
         """unitest"""
         try:
             os.remove('file.json')
@@ -24,21 +24,21 @@ class TestConsole(TestCase):
         """unitest"""
         with patch('sys.stdout', new=StringIO()) as a:
             HBNBCommand().onecmd("create cheking")
-        out = f.getvalue()[:-1]
+        out = a.getvalue()[:-1]
         self.assertEqual(out, "** class doesn't exist **")
     
     def testcre(self):
         """unitest"""
         with patch('sys.stdout', new=StringIO()) as a:
             HBNBCommand().onecmd("create")
-        out = f.getvalue()[:-1]
+        out = a.getvalue()[:-1]
         self.assertEqual(out, "** class name missing **")
 
     def testcreatebasemodel(self):
         """unitest"""
         with patch('sys.stdout', new=StringIO()) as a:
             HBNBCommand().onecmd("basemdl creating")
-        out = f.getvalue()[:-1]
+        out = a.getvalue()[:-1]
         self.assertEqual(type(out), str)
 
     def unittestaa(self):
@@ -46,14 +46,14 @@ class TestConsole(TestCase):
         with patch('sys.stdout', new=StringIO()) as a:
             HBNBCommand().onecmd('create State name="Sfax"')
             HBNBCommand().onecmd('all State')
-        out = f.getvalue()[:-1]
+        out = a.getvalue()[:-1]
         self.assertTrue("'name': 'Sfax'" in out)
 
+     
     def test(self):
-        """uniteste"""
-        with patch('sys.stdout', new=StringIO()) as a:
-            HBNBCommand().onecmd('create Place nbr_of_bedrooms=1 price=5.89')
-            HBNBCommand().onecmd('Places')
-        msg = f.getvalue()[:-1]
-        self.assertTrue("'nbr_of_bedrooms': 1" in msg)
-        self.assertTrue("'price': 5.89" in msg)
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd('create Place nbr_zeb=2 price=5.5')
+            HBNBCommand().onecmd('all Place')
+        out = f.getvalue()[:-1]
+        self.assertTrue("'nbr_zeb': 2" in out)
+        self.assertTrue("'price': 5.5" in out)
