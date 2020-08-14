7cd60fa22307ab42240b162fef2c6392e1e7e4a8
hamdi458 <hamdi.645@gmail.com>
2020-08-14 01:50:10 +0100

    task6

---
+++ models/engine/db_storage.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+import models
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from os import getenv
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+
+class DBStorage:
+    """DBStorage class"""
+    __engine = None
+    __session = None
+ 
+    def __init__(self):
+        """Init"""
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+                                      format(getenv("HBNB_MYSQL_USER"),
+                                             getenv("HBNB_MYSQL_PWD"),
+                                             getenv("HBNB_MYSQL_HOST"),
+                                             getenv("HBNB_MYSQL_DB")),
+                                      pool_pre_ping=True)
+        if getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
+    
+    def all(self, cls=None):
+        """all"""
+        my_dict = {}
+        query = []
+        if cls is not None:
+            query = self.__session.query(cls).all()
+        if cls is None:
+            query += self.__session.query(User).all()
+            query += self.__session.query(State).all()
+            query += self.__session.query(City).all()
+            query += self.__session.query(Amenity).all()
+            query += self.__session.query(Place).all()
+            query += self.__session.query(Review).all()
+
+        for val in query:
+            key = "{}.{}".format(val.__class__.__name__, val.id)
+            my_dict[key] = val
+        return my_dict
+    def new(self, obj):
+        """new"""
+        self.__session.add(obj)
+    def save(self):
+        """save"""
+        self.__session.commit()
+    def delete(self, obj=None):
+        """Delete obj from the current database session."""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Create all tables in the database and initialize a new session."""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+    def close(self):
+        """Close the working SQLAlchemy session."""
+        self.__session.close()
