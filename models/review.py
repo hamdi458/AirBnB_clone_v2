dc5520b37641b7c146ad7239963ee661e927b412
Immaannn2222 <imennaayari@gmail.com>
2020-08-14 22:06:27 +0100

    f

--- models/review.py
+++ models/review.py
@@ -6,7 +6,7 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
