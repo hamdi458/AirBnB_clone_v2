1a111580362f7aa7270da9551b0ec0bd31e50e4b
hamdi458 <hamdi.645@gmail.com>
2020-08-17 20:53:16 +0100

    aa

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
+# Bash script that sets up your web servers for the deployment of web_static.
 sudo apt-get -y update
 sudo apt-get -y install nginx
 mkdir -p /data/web_static/releases/
@@ -13,17 +13,5 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu: /data/
-printf %s "server {
-    listen 80;
-    listen [::]:80 default_server;
-    root   /var/www//html;
-    index index.nginx-debian.html index.nginx-debian.html;
-    location /redirect_me {
-        return 301 http://youtube.com/;
-    }
-    location /hbnb_static {
-    alias /data/web_static/current/;
-    
-    }
-}" > /etc/nginx/sites-available/default
-sudo service nginx restart
+sed -i '46 i\ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}\n' /etc/nginx/sites-enabled/default
+service nginx restart
