309020c8cc853867513ee8190e2118844353fb66
hamdi458 <hamdi.645@gmail.com>
2020-08-17 22:17:24 +0100

    task0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,7 +2,7 @@
 # Bash script that sets up your web servers for the deployment of web_static.
 sudo apt-get -y update
 sudo apt-get -y install nginx
-mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/releases/test 
 mkdir -p /data/web_static/shared/
 echo -e "<html>
   <head>
@@ -11,7 +11,7 @@ echo -e "<html>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu: /data/
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu: /data/
 sudo sed -i '44i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx restart
+service nginx restart
