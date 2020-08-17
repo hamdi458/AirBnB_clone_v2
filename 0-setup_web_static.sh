442ea23f9e4ac62f90b715540d4abf43dab4d34b
hamdi458 <hamdi.645@gmail.com>
2020-08-17 20:56:25 +0100

    aa

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
-echo "<html>
+echo -e "<html>
   <head>
   </head>
   <body>
@@ -13,5 +13,5 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu: /data/
-sed -i '46 i\ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}\n' /etc/nginx/sites-enabled/default
-service nginx restart
+sudo sed -i '44i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
