d3b1dda569b29bfeb03dd5d7ba5fb560f4d3e138
hamdi458 <hamdi.645@gmail.com>
2020-09-02 04:05:35 +0100

    task2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -11,26 +11,28 @@
 """
 from flask import Flask
 
-
-@app.route("/", strict_slashes=False)
-def hello():
-    """'Hello HBNB!'"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """'HBNB'"""
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def css(txt):
-    """txt"""
-    txt = txt.replace("_", " ")
-    return "C {}".format(txt)
+app = Flask(__name__)
 
 
 if __name__ == "__main__":
     app = Flask(__name__)
+
+    @app.route("/", strict_slashes=False)
+    def hello():
+        """'Hello HBNB!'"""
+        return "Hello HBNB!"
+
+
+    @app.route("/hbnb", strict_slashes=False)
+    def hbnb():
+        """'HBNB'"""
+        return "HBNB"
+
+
+    @app.route("/c/<text>", strict_slashes=False)
+    def cs(txt):
+        """txt"""
+        txt = txt.replace("_", " ")
+        return "C {}".format(txt)
+    
     app.run(host="0.0.0.0")
