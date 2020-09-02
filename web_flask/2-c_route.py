181d173c07658c68b04a77a059990a93e7945172
hamdi458 <hamdi.645@gmail.com>
2020-09-02 04:21:29 +0100

    task2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""flaskbzhamd"""
+"""Flask"""
 from flask import Flask
+app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+def hello():
+    """Hello HBNB"""
+    return 'Hello HBNB!'
 
-    @app.route("/", strict_slashes=False)
-    def hello():
-        """'Hello HBNB!'"""
-        return "Hello HBNB!"
 
-    @app.route("/hbnb", strict_slashes=False)
-    def hbnb():
-        """'HBNB'"""
-        return "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """HBNB"""
+    return 'HBNB'
 
-    @app.route("/c/<text>", strict_slashes=False)
-    def cs(txt):
-        """txt"""
-        return 'C ' + txt.replace('_', ' ')
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """text """
+    return ("C %s" % text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
