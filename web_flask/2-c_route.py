51b31580d6aea905f7a821b35f8f3bdf781dac1b
hamdi458 <hamdi.645@gmail.com>
2020-09-02 04:17:16 +0100

    task2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""flask
-"""
+"""flaskbzhamd"""
 from flask import Flask
 
 
@@ -15,11 +14,10 @@ if __name__ == "__main__":
     @app.route("/hbnb", strict_slashes=False)
     def hbnb():
         """'HBNB'"""
-        return "HBNB!"
+        return "HBNB"
 
     @app.route("/c/<text>", strict_slashes=False)
     def cs(txt):
         """txt"""
-        txt = txt.replace("_", " ")
-        return "C {}".format(txt)
+        return 'C ' + txt.replace('_', ' ')
     app.run(host="0.0.0.0", port=5000)
